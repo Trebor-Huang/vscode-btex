ff283af118f6525befa1f917bf21f3337c4f697f
Trebor-Huang <2300936257@qq.com>
2022-06-25 15:44:28 +0800

    Initial setup

---
+++ README.md
@@ -0,0 +1,34 @@
+# vscode-btex README
+
+(...)
+
+## Features
+
+- [ ] Syntax highlights.
+- [ ] Compiles the bTeX file and show the preview either in real time or on save.
+  - [X] Displays converted result assuming the server is running.
+  - [ ] Cache KaTeX modules.
+  - [ ] Run node.js module process to start up bTeX engine automatically (or disable that as a server).
+  - [ ] Display HTML properly.
+  - [ ] Keep track of processes and panels, so that they are unique.
+  - [ ] Handle errors.
+- [ ] Exports bTeX to html.
+
+## Requirements
+
+- Install bTeX and TikZ2svg. (...)
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+## Release Notes
---
+++ src/extension.ts
@@ -0,0 +1,65 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as http from 'http';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('bTeX: Active.');  // TODO log context
+    let disposable = vscode.commands.registerCommand('vscode-btex.compile',
+        () => { compile(context.extensionPath); });
+    context.subscriptions.push(disposable);
+}
+
+function compile(extensionPath: string) {
+    let doc = vscode.window.activeTextEditor?.document;
+    if (doc === undefined) {
+        return;
+    }
+    let text = doc.getText();
+    var request = http.request({
+        host: 'localhost',
+        port: 7200,
+        path: '/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    },
+        function (response) {
+            var data = '';
+            response.on('data', function (chunk) { data += chunk; });
+            response.on('end', function () {
+                // console.log(data);
+                render(extensionPath, JSON.parse(data));
+            });
+        });
+    request.write(JSON.stringify({code: text}));
+    request.end();
+}
+
+function render(extensionPath: string, data: { html: string; }) {
+    const panel = vscode.window.createWebviewPanel(
+        'bTeXpreview',
+        'Preview bTeX',
+        vscode.ViewColumn.Beside,
+        {}
+    );
+    const csssrc = vscode.Uri.file(
+        path.join(extensionPath, 'resources', 'base.css')
+    );
+
+    panel.webview.html = `<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css">
+    <link rel="stylesheet" href="${csssrc}">
+    <title>bTeX Preview</title>
+</head>
+
+<body>
+${data.html}
+</body>
+    `;
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
